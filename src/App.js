2a298595f672302ada1130aff1727a80db575217
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-07-17 14:39:02 -0700

    addingaclock

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
-
-import Clock from "./Clock";
-
+import React, { useEffect, useState } from "react";
+import Clock  from "./Clock";
 function App() {
   const [showClock, setShowClock] = useState(true);
+  const [time ,setTime]=useState(new Date())
+useEffect(()=>{
+const timer=setInterval(() => {
+  setTime(new Date())
+}, 1000);
+return ()=>{
+  clearInterval(timer)
+}
 
+},[])
   return (
     <div>
-      {showClock ? <Clock /> : null}
-      <button onClick={() => setShowClock(!showClock)}>Toggle Clock</button>
+{time.toString()}
     </div>
   );
 }
